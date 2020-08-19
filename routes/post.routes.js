838d40c35bde1e8461b19c07226bafc7f1acfcdf
Sergey Krotkih <sergey.krotkig@gmail.com>
2020-08-19 09:19:55 +0300

    fix add new post request

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,8 +1,5 @@
 const {Router} = require('express')
-const config = require('config')
-const shortid = require('shortid')
 const Post = require('../models/Post')
-const auth = require('../middleware/auth.middleware')
 const router = Router()
 
 // Get all posts
@@ -10,16 +7,13 @@ const router = Router()
 // router.get('/posts', auth, async (req, res) => {
 router.get('/posts', async (req, res) => {
   try {
-
-    console.log("allPosts request");
-
     const allPosts = await Post.find({})
-
-    console.log("allPosts", allPosts);
-
     res.status(200).json(allPosts)
-  } catch (e) {
-    res.status(500).json({ message: 'Something went wrong. Please try it again.' })
+  } catch (error) {
+
+    console.log('500', error);
+
+    res.status(500).json({ message: `${error}` })
   }
 })
 
@@ -29,19 +23,22 @@ router.post('/post', async (req, res) => {
 
     const {post} = req.body
 
-    console.log(post);
+    console.log('source', post);
 
     const postObj = new Post(post);
 
-    console.log(postObj);
+    console.log('obj post=', postObj);
 
     await postObj.save();
 
     console.log("SAVED!!!");
 
-    res.status(200).json({ postObj })
-  } catch (e) {
-    res.status(500).json({ message: '//post request: something went wrong. Please try it again.' })
+    res.status(200).json(postObj);
+  } catch (error) {
+
+    console.log('500', error);
+
+    res.status(500).json({ message: `${error}` })
   }
 })
 
@@ -50,8 +47,11 @@ router.get('/', async (req, res) => {
   try {
     const usersPosts = await Post.find({ owner: req.user.userId })
     res.status(200).json(usersPosts)
-  } catch (e) {
-    res.status(500).json({ message: 'Something went wrong. Please try it again.' })
+  } catch (error) {
+
+    console.log('500', error);
+
+    res.status(500).json({ message: `${error}` })
   }
 })
 
@@ -60,8 +60,11 @@ router.get('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id)
     res.status(200).json(post)
-  } catch (e) {
-    res.status(500).json({ message: 'Something went wrong. Please try it again.' })
+  } catch (error) {
+
+    console.log('500', error);
+
+    res.status(500).json({ message: `${error}` })
   }
 })
 
